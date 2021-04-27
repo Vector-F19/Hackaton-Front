32c343280e8565f96321360e37084e2f630f7f77
rodrigo7023 <rodrigoo.89@hotmail.com>
2021-04-26 21:25:45 -0300

    Front

---
+++ js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(function() {
+  function init() {
+    var router = new Router([
+      new Route("login", "login.html", true),
+      new Route("dash", "dash.html"),
+      new Route("teste", "teste.html")
+      ]);
+  }
+  init();
+})();
